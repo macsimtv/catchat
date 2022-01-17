e5bf86d04e25a7393f852c420251e00b330cf074
romain  buisson <romainbuisson@MacBook-Pro-de-romain.local>
2022-01-17 16:55:18 +0100

    mise en place de la connection

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,16 @@
 <script setup>
-import { ref } from 'vue'
+import { ref,router } from 'vue'
+import  auth  from '../services/module/auth'
 
+let username = ref("");
+let password = ref("");
+
+async function login(){
+  const isConnected = await auth.login({username: username.value, password: password.value})
+  if (isConnected){
+    router.push('home')
+  }
+}
 </script>
 
 <template>
@@ -9,9 +19,9 @@ import { ref } from 'vue'
     <h1 class="login-page__title">Connectez-vous</h1>
       <div class="login-page__form">
         <form action="#" class="login-form">
-          <input type="text" class="login-page__input" placeholder="Pseudo" />
-          <input type="text" class="login-page__input" placeholder="Mot de passe" />
-          <button type="submit" class="login-page__button">Connexion</button>
+          <input type="text" class="login-page__input" v-model="username" placeholder="Pseudo" />
+          <input type="password" class="login-page__input" v-model="password" placeholder="Mot de passe" />
+          <button type="submit" @click.prevent="login" class="login-page__button">Connexion</button>
         </form>
       </div>
     </div>
