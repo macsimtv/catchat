e905f3d0812834b7cfab45b7fc188b99c74772fb
romain  buisson <romainbuisson@MacBook-Pro-de-romain.local>
2022-01-18 13:29:34 +0100

    fix du bug de bertyn

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,19 +9,11 @@ import HomeServerList from "../components/section/HomeServerList.vue";
   <Header />
   <main class="home">
     <div class="home__container">
-<<<<<<< HEAD
-      <HomeServerList />
-      <div style="flex: 1">
-        <!-- ChatBox -->
-      </div>
-      <HomeUserList />
-=======
       <HomeServerList/>
       <section style="flex: 1;">
         <!-- ChatBox -->
       </section>
       <HomeUserList/>
->>>>>>> ea36eca6d5c81f7e571a984319e0f9db194b3db2
     </div>
   </main>
 </template>
