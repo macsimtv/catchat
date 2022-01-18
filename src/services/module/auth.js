0fec719d54261379190158cdc6acb8b02bbdcbc9
macsimtv <maximelecouturier@gmail.com>
2022-01-19 00:59:51 +0100

    Gestion d'erreur du formulaire de connexion

--- src/services/module/auth.js
+++ src/services/module/auth.js
@@ -2,14 +2,17 @@ import apiClient from "../http-comon";
 export default {
   //Auth
   async login(form) {
-    const res = await apiClient.post("/login", form);
-    if (res.status == 200){
-      localStorage['token'] = res.data.token
-      localStorage['token_date'] = new Date().getTime()
-      localStorage['username'] = form.username
-      return true
-    }
-    return false
+    try {
+      const res = await apiClient.post("/login", form);
+      if (res.status == 200) {
+        localStorage["token"] = res.data.token;
+        localStorage["token_date"] = new Date().getTime();
+        localStorage["username"] = form.username;
+        return true;
+      }
+    } catch (err) {}
+
+    return false;
   },
 
   reconnection() {
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -8,11 +8,17 @@ const router = useRouter();
 let username = ref("");
 let password = ref("");
 
+let error = ref("");
+
 async function login(){
+  error.value = "";
+
   const isConnected = await auth.login({username: username.value, password: password.value})
   if (isConnected){
     router.push({ name: 'Home' });
   }
+
+  error.value = "Mauvaises informations de connexion";
 }
 </script>
 
@@ -22,6 +28,9 @@ async function login(){
     <h1 class="login-page__title">Connectez-vous</h1>
       <div class="login-page__form">
         <form action="#" class="login-form">
+          <p style="color: red; margin-bottom: 10px" v-if="error">
+            {{ error }}
+          </p>
           <input type="text" class="login-page__input" v-model="username" placeholder="Pseudo" />
           <input type="password" class="login-page__input" v-model="password" placeholder="Mot de passe" />
           <button type="submit" @click.prevent="login" class="login-page__button">Connexion</button>
