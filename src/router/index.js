013b97f918fab611150276ce0de220b95cff0d51
macsimtv <maximelecouturier@gmail.com>
2022-01-19 00:50:03 +0100

    [Update] Ajout d'un middleware si la personne est connecté pour ne pas aller sur la page login

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '/src/views/Home.vue'
 import Login from '/src/views/Login.vue'
+
 const routes = [
     {
         path: '/',
@@ -12,23 +13,22 @@ const routes = [
         name: 'Login',
         component: Login,
     },
-]
+];
+
 const router = createRouter({
     history: createWebHistory(),
     routes,
-})
+});
 
 /* AUTH */
 router.beforeEach((to, from, next) => {
-    if (to.fullPath.includes('login')){
-        return next()
-    }
+    if (localStorage['token'].length && !(new Date().getTime() - localStorage['token_date']) < (10800 * 1000) && to.name == 'Login') return router.push({ name: 'Home' })
 
-    if (localStorage['token'] && localStorage['token'].length && (new Date().getTime() - localStorage['token_date']) < (10800 * 1000)){
-        return next()
-    }
+    if (to.name == 'Login') return next();
+
+    if (localStorage['token'].length && (new Date().getTime() - localStorage['token_date']) < (10800 * 1000)) return next();
 
     return router.push({ name: 'Login' });
-   
-  })
+  });
+
 export default router
