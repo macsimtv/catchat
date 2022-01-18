799bf1896685eab175e5c27fdb9b70055ca71013
Bertyn Boulikou <bboulikou@gmail.com>
2022-01-18 14:05:46 +0100

    ajout crud channel services

--- src/services/module/channel.js
+++ src/services/module/channel.js
@@ -1,2 +1,45 @@
 import apiClient from "../http-common";
-export default {};
+export default {
+  async listOfChannel() {
+    return apiClient.get(`/msg/protected/user/channels`, {
+      headers: {
+        authorization: "Bearer " + localStorage["token"],
+      },
+    });
+  },
+
+  async createChannel(channel) {
+    return apiClient.post(
+      `/msg/protected/channel`,
+      {
+        headers: {
+          authorization: "Bearer " + localStorage["token"],
+        },
+      },
+      channel
+    );
+  },
+
+  async updateMetaChannel(id, channel) {
+    return apiClient.get(
+      `/msg/protected/channel/${id}/update_metadata`,
+      {
+        headers: {
+          authorization: "Bearer " + localStorage["token"],
+        },
+      },
+      channel
+    );
+  },
+  async banUserFromChannel(idChannel, username, channel) {
+    return apiClient.delete(
+      `/msg/protected/channel/${idChannel}/user/${username}'`,
+      {
+        headers: {
+          authorization: "Bearer " + localStorage["token"],
+        },
+      },
+      channel
+    );
+  },
+};
