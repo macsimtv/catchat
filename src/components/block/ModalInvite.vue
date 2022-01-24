434abadfb6250d34dcb59c70768f977bb754226b
macsimtv <maximelecouturier@gmail.com>
2022-01-24 16:06:14 +0100

    Invite users to a server

---
+++ src/components/block/ModalInvite.vue
@@ -0,0 +1,54 @@
+<script setup>
+import { ref, inject } from "vue";
+import channelServices from "../../services/module/channel";
+
+const { state, setStateProp } = inject("state");
+
+const emit = defineEmits(['onCloseModalInvite'])
+const userInput = ref('');
+
+async function onSend() {
+  const userAdded = await channelServices.addUserInAChannel(state.currentChannel.id, userInput.value);
+  
+  let userList = state.currentChannel.users;
+  userList.push(userInput.value);
+
+  userInput.value = "";
+  onCloseModalInvite();
+}
+
+function onCloseModalInvite() {
+  emit('onCloseModalInvite');
+}
+
+defineProps({
+  isOpen: Boolean,
+})
+
+</script>
+
+<template>
+  <teleport to="body">
+    <transition name="fade">
+      <div v-if="isOpen" class="modal-invite">
+        <div class="modal-invite__container">
+          <div class="modal-invite__body">
+            <h2 class="modal-invite__title">Ajouter un utilisateur</h2>
+            <form @submit.prevent="onSend" class="modal-invite__form">
+              <input
+                v-model="userInput"
+                type="text"
+                placeholder="Nom de l'utilisateur"
+              />
+              <button type="submit">Ajouter</button>
+            </form>
+          </div>
+          <div
+            @click="onCloseModalInvite"
+            class="modal__background"
+          ></div>
+        </div>
+      </div>
+    </transition>
+  </teleport>
+</template>
