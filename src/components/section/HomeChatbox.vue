f5e6ddc857dffee73b77faaea4b7a320d58b0efe
macsimtv <maximelecouturier@gmail.com>
2022-01-24 11:07:45 +0100

    Ajout de la barre du haut

--- src/components/section/HomeChatbox.vue
+++ src/components/section/HomeChatbox.vue
@@ -76,23 +76,52 @@ const send = async () => {
 
 <template>
   <section class="home-chatbox">
+    <div class="server-bar">
+      <div class="server-bar__container">
+        <h2 class="server-bar__title">Nom du server</h2>
+        <div class="server-bar__menu">
+          <button class="server-bar__settings">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
+              />
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+    </div>
     <div class="home-chatbox__container">
       <Message v-for="(msg, index) in messages" :msg="msg" :key="index" />
     </div>
     <form @submit.prevent="send">
-        <div class="form-controle">
-          <input placeholder="Aa" v-model="message" />
-          <button @click="send">
-            <img src="img/send.png" />
-          </button>
-          <discord-picker
-            :value="value"
-            gif-format="md"
-            @update:value="value = $event"
-            @emoji="setEmoji"
-            @gif="setGif"
-          />
-        </div>
-      </form>
+      <div class="form-controle">
+        <input placeholder="Aa" v-model="message" />
+        <button @click="send">
+          <img src="img/send.png" />
+        </button>
+        <discord-picker
+          :value="value"
+          gif-format="md"
+          @update:value="value = $event"
+          @emoji="setEmoji"
+          @gif="setGif"
+        />
+      </div>
+    </form>
   </section>
 </template>
