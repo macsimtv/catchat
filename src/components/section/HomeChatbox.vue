d1eb36bb35cc7b6f2770c12247dbce1dfc48dbad
macsimtv <maximelecouturier@gmail.com>
2022-01-24 11:45:58 +0100

    Ajout du scroll automatique vers le bas

--- src/components/section/HomeChatbox.vue
+++ src/components/section/HomeChatbox.vue
@@ -1,6 +1,6 @@
 <script setup>
 import Message from "../block/Message.vue";
-import { ref, inject } from "vue";
+import { ref, inject, onUpdated } from "vue";
 const { state, setStateProp } = inject("state");
 import DiscordPicker from "vue3-discordpicker";
 import MessagesService from "../../services/module/messages";
@@ -16,68 +16,34 @@ function setGif(gif) {
 
 const textInput = ref("");
 
-const messages = [
-  {
-    channel_id: 54,
-    timestamp: 1641124598,
-    author: "jsos10",
-    content: {
-      Text: "Je suis un message",
-      Image: null,
-    },
-  },
-  {
-    channel_id: 13,
-    timestamp: 1641124598,
-    author: "mlecouturier",
-    content: {
-      Text: "Ceci est un message test",
-      Image: null,
-    },
-  },
-  {
-    channel_id: 54,
-    timestamp: 1641124598,
-    author: "rbuisson",
-    content: {
-      Text: "Je suis un autre message",
-      Image: null,
-    },
-  },
-  {
-    channel_id: 54,
-    timestamp: 1641124598,
-    author: "rbuisson",
-    content: {
-      Text: "Je suis un autre messageJe suis un autre messageJe suis un autre messageJe suis un autre messageJe suis un autre messageJe suis un autre messageJe suis un autre message",
-      Image: null,
-    },
-  },
-  {
-    channel_id: 13,
-    timestamp: 1641124598,
-    author: "mlecouturier",
-    content: {
-      Text: "Yo comment tu vas ?",
-      Image: null,
-    },
-  },
-];
-
-const send = async () => {
- const isMessageSend = await MessagesService.sendMessage(state.currentChannel.id, {Text: textInput.value})
-  if (isMessageSend){
-    textInput.value = ""
-  }else{
-    alert('le message ne s\'est pas envoyer')
+const send = async (e) => {
+  const isMessageSend = await MessagesService.sendMessage(
+    state.currentChannel.id,
+    { Text: textInput.value }
+  );
+  if (isMessageSend) {
+    textInput.value = "";
+  } else {
+    alert("le message ne s'est pas envoyer");
   }
+
+  onScrollBottom();
 };
+
+onUpdated(() => {
+  onScrollBottom();
+})
+
+const onScrollBottom = () => {
+  let messageContainer = document.querySelector(".home-chatbox__container");
+  messageContainer.scrollTop = messageContainer.scrollHeight;
+}
 
 </script>
 
 <template>
   <section class="home-chatbox">
-    <ServerBar/>
+    <ServerBar />
     <div class="home-chatbox__container">
       <Message v-for="(msg, index) in state.messages" :msg="msg" :key="index" />
     </div>
