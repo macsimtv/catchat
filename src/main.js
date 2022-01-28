dc6cca35e840a60681bde89c815eec41626a419c
bertyn99 <bboulikou@gmail.com>
2022-01-28 23:37:33 +0100

    enlever les commentaire

--- src/main.js
+++ src/main.js
@@ -5,19 +5,6 @@ import GStore from "./store/index";
 import { directive } from "./utils/vue-directive-clickOutside.js";
 createApp(App)
   .provide("state", GStore)
-  /* .directive("click-outside", {
-    beforeMount(el, binding, vnode) {
-      el.clickOutsideEvent = function(event) {
-        if (!(el === event.target || el.contains(event.target))) {
-          binding.value(event, el);
-        }
-      };
-      document.body.addEventListener('click', el.clickOutsideEvent);
-    },
-    unmounted(el) {
-      document.body.removeEventListener('click', el.clickOutsideEvent);
-    }
-  }) */
   .directive("click-outside", directive)
   .use(router)
   .mount("#app");
